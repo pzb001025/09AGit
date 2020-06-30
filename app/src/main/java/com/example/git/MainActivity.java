3f3d0f5d0e261e62bdf2c3c737ffc532009ca4e0
pzb001025 <66061355+pzb001025@users.noreply.github.com>
2020-06-30 11:05:48 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -11,10 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a = 1;
-        int b = 2;
-        int c = 3;
-
-
+        String str1 = "a";
+        String str2 = "b";
+        String str3 = "c";
     }
 }
