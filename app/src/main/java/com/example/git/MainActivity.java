7edc528589f5e9b3b9a43122d3fe78783604adb0
pzb001025 <1275776996@qq.com>
2020-06-30 11:14:45 +0800

    aa

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -11,10 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        Log.d("TAG", "onCreate: "+c);
+        String a = "a";
+        String b = "b";
+        String c = "c";
     }
 }
